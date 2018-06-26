8f5755b9c48e36b766b0fc3e03a62c5b78c4a8d2
Kose-i <tamura.kosei905@mail.kyutech.jp>
2018-06-26 19:39:27 +0900

    Add struct function

--- class.c
+++ class.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
-void print(){
-  printf("test");
+int print(int a){
+  printf("test\n");
+  return 4;
 }
 
 struct test{
-  void test = print;
+  int (*tre)(int);
 };
+//int test.tre(int global) {
+//  return global;
+//}
 
 
 int main(int argc, char** argv) {
   struct test t;
+  t.tre = print;
+  printf("%d\n", t.tre(3));
 }
