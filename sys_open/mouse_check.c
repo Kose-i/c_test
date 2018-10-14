2d1d07770839e4618f547e27c0a69e398285cfc5
Kose-i <tamura.kosei905@mail.kyutech.jp>
2018-10-14 14:06:44 +0900

    add check_usb

---
+++ sys_open/mouse_check.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <linux/input.h>
+#include <linux/uinput.h>
+
+#define die(str) do{perror(str);exit(EXIT_FAILURE);}while(0)
+
+int main(int argc, char** argv) {
+  int fd = open("/dev/input/event0",O_WRONLY);
+  return 0;
+}
