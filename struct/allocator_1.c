ef0b63769062dbdb5ca19cc20512ca63abd71437
Kose-i <tamura.kosei905@mail.kyutech.jp>
2018-12-25 15:56:06 +0900

    Add struct memory

---
+++ struct/allocator_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+typedef struct Tmp{
+  char c_1;
+  char c_2;
+  int i_1;
+  int i_2;
+}Tmp;
+
+int main(int argc, char** argv) {
+  Tmp tmp;
+  printf("tmp.c_1:%p\n", &tmp.c_1);
+  printf("tmp.c_2:%p\n", &tmp.c_2);
+  printf("tmp.i_1:%p\n", &tmp.i_1);
+  printf("tmp.i_2:%p\n", &tmp.i_2);
+}
